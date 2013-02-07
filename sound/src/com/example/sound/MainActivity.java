10c2a5756edd21ca40244823c3cc28463f06fd76
simonnicolas55@gmail.com <simonnicolas55@gmail.com@04a9d38c-7ece-b0a5-ac90-e286bd55b617>
2013-02-07 21:42:06 +0000

    Allez on veut du reve

--- sound/src/com/example/sound/MainActivity.java
+++ sound/src/com/example/sound/MainActivity.java
@@ -1,7 +1,7 @@
 package com.example.sound;
 
 import android.app.Activity;
-//import android.graphics.Color;
+import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -11,21 +11,15 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-//import android.widget.RelativeLayout;
+import android.widget.RelativeLayout;
 import android.widget.GridView;
 import android.widget.AdapterView.OnItemClickListener;
 
 
 public class MainActivity extends Activity 
 {
-	protected	boolean			_loadComplete = false;
-	final		MainActivity	start = this;
-	//private		int				_time = 5000;
-	private		Thread			SplashScreenThread;
-	//private		DrawRectangle	dR1;
-	//private		DrawRectangle	dR2;
-	//private		DrawRectangle	dR3;
-	//private		RelativeLayout	_mainLayout;
+	private		DrawRectangle	dR0, dR1, dR2, dR3, dR4, dR5, dR6, dR7;
+	private		RelativeLayout	_mainLayout;
 	
 	GridView	GridV;
 	
@@ -34,107 +28,57 @@ public class MainActivity extends Activity
 		"6", "7", "8", "9", "10",
 		"11", "12", "13", "14", "15",
 		"16"};
-	
-	OnClickListener				b1click;
-	OnClickListener				b2click;
-	OnClickListener				b3click;
-	OnClickListener				b4click;
-	OnClickListener				b5click;
-	OnClickListener				b6click;
-	OnClickListener				b7click;
-	OnClickListener				b8click;
 	MyMusicThread[]				_musicThreadsArray;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) 
-    {
-    	super.onCreate(savedInstanceState);
-    	setContentView(R.layout.activity_main);
-    	
-    	SplashScreenThread = new Thread() {
-		@Override
-		public void run()
-		{
-			try 
-			{
-				synchronized (this)
-				{
-//					dR1 = new DrawRectangle(start, Color.BLACK, 0, 100, 400, 150);
-//					dR2 = new DrawRectangle(start, Color.BLUE, 0, 150, 350, 200);
-//					dR3 = new DrawRectangle(start, Color.GREEN, 0, 200, 375, 250);
-//					_mainLayout.addView(dR1);
-//					_mainLayout.addView(dR2);
-//					_mainLayout.addView(dR3);
-					_musicThreadsArray = new MyMusicThread[8];
-					_musicThreadsArray[0] = new MyMusicThread(MediaPlayer.create(start, R.raw.up));
-					_musicThreadsArray[1] = new MyMusicThread(MediaPlayer.create(start, R.raw.dwqdwqcdcc));
-					_musicThreadsArray[2] = new MyMusicThread(MediaPlayer.create(start, R.raw.dwqwdqdwq));
-					_musicThreadsArray[3] = new MyMusicThread(MediaPlayer.create(start, R.raw.edjdfj));
-					_musicThreadsArray[4] = new MyMusicThread(MediaPlayer.create(start, R.raw.eeee));
-					_musicThreadsArray[5] = new MyMusicThread(MediaPlayer.create(start, R.raw.ewew));
-					_musicThreadsArray[6] = new MyMusicThread(MediaPlayer.create(start, R.raw.tat));
-					_musicThreadsArray[7] = new MyMusicThread(MediaPlayer.create(start, R.raw.titi));
-					
-					_musicThreadsArray[0].start();
-					_musicThreadsArray[1].start();
-					_musicThreadsArray[2].start();
-					_musicThreadsArray[3].start();
-					_musicThreadsArray[4].start();
-					_musicThreadsArray[5].start();
-					_musicThreadsArray[6].start();
-					_musicThreadsArray[7].start();
-				}
+	OnClickListener				splashListener = new OnClickListener(){
+		public void onClick(View v){
+			onLife();
 			}
-			catch (Exception e) {}
-			finally
-			{
-				start._loadComplete = true;
-				System.out.println("LALALA Thread died OK?");
-			}
-		}
 		};
-		SplashScreenThread.start();
-
-		b1click = new OnClickListener(){
+	OnClickListener				b1click = new OnClickListener(){
 			public void onClick(View v){
 				_musicThreadsArray[0].run();
 				}
 			};
-        b2click = new OnClickListener(){
+	OnClickListener				b2click = new OnClickListener(){
           	public void onClick(View v){
            		_musicThreadsArray[1].run();
            	}
         };
-        b3click = new OnClickListener(){
+    OnClickListener        		b3click = new OnClickListener(){
            	public void onClick(View v){
            		_musicThreadsArray[2].run();
            	}
         };
-        b4click = new OnClickListener(){
+    OnClickListener        		b4click = new OnClickListener(){
            	public void onClick(View v){
            		_musicThreadsArray[3].run();
            	}
         };
-        b5click = new OnClickListener(){
+    OnClickListener        		b5click = new OnClickListener(){
            	public void onClick(View v){
            		_musicThreadsArray[4].run();
            	}
         };
-        b6click = new OnClickListener(){
+    OnClickListener        		b6click = new OnClickListener(){
            	public void onClick(View v){
            		_musicThreadsArray[5].run();
            	}
         };
-        b7click = new OnClickListener(){
+    OnClickListener       		b7click = new OnClickListener(){
            	public void onClick(View v){
            		_musicThreadsArray[6].run();
            	}
         };
-        b8click = new OnClickListener(){
+    OnClickListener        		b8click = new OnClickListener(){
            	public void onClick(View v){
            		_musicThreadsArray[7].run();
            	}
         };
+
+	public void onLife()
+	{
+    	setContentView(R.layout.activity_main);
         Button button1 = (Button) findViewById(R.id.button1);
         Button button2 = (Button) findViewById(R.id.button2);
         Button button3 = (Button) findViewById(R.id.button3);
@@ -161,7 +105,6 @@ public class MainActivity extends Activity
 			}
 		});
 
-        while (!_loadComplete); // A changer dès que possible!
         button1.setOnClickListener(b1click);
         button2.setOnClickListener(b2click);
         button3.setOnClickListener(b3click);
@@ -170,6 +113,59 @@ public class MainActivity extends Activity
         button6.setOnClickListener(b6click);
         button7.setOnClickListener(b7click);
         button8.setOnClickListener(b8click);
-    	System.out.println("LALALA Tout yé bon ici...");
+	}
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) 
+    {
+    	super.onCreate(savedInstanceState);
+    	setContentView(R.layout.startscreen);
+    	
+    	System.out.println("LALALA Starting test...");
+		_musicThreadsArray = new MyMusicThread[8];
+    	System.out.println("LALALA Retrieving layout...");
+		_mainLayout = (RelativeLayout) findViewById(R.id.splashScreen);
+		
+		_musicThreadsArray[0] = new MyMusicThread(MediaPlayer.create(this, R.raw.up));
+		_musicThreadsArray[0].start();
+		dR0 = new DrawRectangle(this, Color.BLACK, 0, 0, 400, 50);
+		_mainLayout.addView(dR0);
+
+		_musicThreadsArray[1] = new MyMusicThread(MediaPlayer.create(this, R.raw.dwqdwqcdcc));
+		_musicThreadsArray[1].start();
+		dR1 = new DrawRectangle(this, Color.BLUE, 0, 50, 350, 100);
+		_mainLayout.addView(dR1);
+
+		_musicThreadsArray[2] = new MyMusicThread(MediaPlayer.create(this, R.raw.dwqwdqdwq));
+		_musicThreadsArray[2].start();
+		dR2 = new DrawRectangle(this, Color.GREEN, 0, 100, 375, 150);
+		_mainLayout.addView(dR2);
+		
+		_musicThreadsArray[3] = new MyMusicThread(MediaPlayer.create(this, R.raw.edjdfj));
+		_musicThreadsArray[3].start();
+		dR3 = new DrawRectangle(this, Color.WHITE, 0, 150, 450, 200);
+		_mainLayout.addView(dR3);
+		
+		_musicThreadsArray[4] = new MyMusicThread(MediaPlayer.create(this, R.raw.eeee));
+		_musicThreadsArray[4].start();
+		dR4 = new DrawRectangle(this, Color.BLACK, 0, 200, 400, 250);
+		_mainLayout.addView(dR4);
+		
+		_musicThreadsArray[5] = new MyMusicThread(MediaPlayer.create(this, R.raw.ewew));
+		_musicThreadsArray[5].start();
+		dR5 = new DrawRectangle(this, Color.BLUE, 0, 250, 350, 300);
+		_mainLayout.addView(dR5);
+		
+		_musicThreadsArray[6] = new MyMusicThread(MediaPlayer.create(this, R.raw.tat));
+		_musicThreadsArray[6].start();
+		dR6 = new DrawRectangle(this, Color.GREEN, 0, 300, 375, 350);
+		_mainLayout.addView(dR6);
+		
+		_musicThreadsArray[7] = new MyMusicThread(MediaPlayer.create(this, R.raw.titi));
+		_musicThreadsArray[7].start();
+		dR7 = new DrawRectangle(this, Color.WHITE, 0, 350, 450, 400);
+		_mainLayout.addView(dR7);
+
+		_mainLayout.setOnClickListener(splashListener);
     }
 }
